d6d9ec004c4a68f754ac8cba8a1e050e99503c6a
Rana <ranaelemam@outlook.com>
2024-07-15 09:46:49 +0300

    system design + initial routes

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_adv_app/core/routing/routes.dart';
+import 'package:flutter_adv_app/features/login/ui/widgets/login_screen.dart';
+import 'package:flutter_adv_app/features/onboarding/onborading_screen.dart';
+
+class AppRouter {
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const onBoardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
