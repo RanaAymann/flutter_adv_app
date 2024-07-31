bb6d44cd42af341c4ee4fa320769895bf63f0a57
Rana Ayman <ranaelemam@outlook.com>
2024-07-31 09:56:41 +0300

    Home screen UI + widgets

---
+++ lib/features/home/widgets/doctors_speciality_list_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_adv_app/core/helpers/spacing.dart';
+import 'package:flutter_adv_app/core/theming/colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../../../core/theming/styles.dart';
+
+class DoctorsSpecialtyListView extends StatelessWidget {
+  const DoctorsSpecialtyListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100.h,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 8,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsetsDirectional.only(start: index == 0 ? 0 : 24.w),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 28,
+                  backgroundColor: ColorsManager.lightBlue,
+                  child: SvgPicture.asset(
+                    'assets/svgs/general_speciality.svg',
+                    height: 40.h,
+                    width: 40.w,
+                  ),
+                ),
+                verticalSpace(8),
+                Text(
+                  'Rana',
+                  style: TextStyles.font12DarkBlueRegular,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
